e930ce9b71d994ab43e9fb36ed6a7f1462340842
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-06-05 15:25:40 -0500

    Add contrast image options

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ const fullConfig: ResolvedConfig<Config> = resolveConfig(tailwindConfig)
 export default function Home() {
   const [currentColor, setCurrentColor] = useState(0)
   const [currentHex, setCurrentHex] = useState('')
+  const [contrastImage, setContrastImage] = useState('')
 
   const colors = [
     'bg-cherry-red',
@@ -28,16 +29,36 @@ export default function Home() {
     setCurrentColor(parseInt(event.target.value))
     setCurrentHex(fullConfig.theme.colors[colors[currentColor].slice(3)])
   }
+
+  const handleContrastImage = (event:any) =>{
+    setContrastImage(event.target.value)
+    console.log(contrastImage)
+  }
   
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className={`flex min-h-screen flex-col items-center justify-center p-24 ${contrastImage} bg-center bg-no-repeat`}>
+      <div className={"bg-red-100 p-10 rounded-md flex items-center flex-col"}>
+        <p>Select Contrast Image</p>
+        <form onChange={event => handleContrastImage(event)} className={'flex'}>
+          <input type="radio" id='trees' value='bg-trees' name='contrast_image'></input>
+          <label className={"p-5"} htmlFor='trees'>trees</label>
+          <input type="radio" id='moon' value='bg-moon' name='contrast_image'></input>
+          <label className={"p-5"} htmlFor='moon'>moon</label>
+          <input type="radio" id='texture' value='bg-texture' name='contrast_image'></input>
+          <label className={"p-5"} htmlFor='texture'>texture</label>
+          <input type="radio" id='none' value='' name='contrast_image'></input>
+          <label className={"p-5"} htmlFor='none'>no image</label>
+        </form>
+      </div>
       <div id='circle' className={`rounded-full h-56 w-56 flex m-8 items-center justify-center ${colors[currentColor]}`}></div>
-      <h1 className={`text-${colors[currentColor].slice(3)}`}>{currentHex}</h1>
-      <input type="range" id="slider" name="slider"
-         min="0" max="11" onChange={event => getColorValue(event)}>
-      </input>
-      <label htmlFor="slider">Choose Color</label>
+      <div className={"bg-red-100 p-10 rounded-md"}>
+        <h1 className={`text-${colors[currentColor].slice(3)}`}>{currentHex}</h1>
+        <input type="range" id="slider" name="slider"
+          min="0" max="11" onChange={event => getColorValue(event)}>
+        </input>
+        <label htmlFor="slider">Choose Color</label>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 10% 50%, var(--tw-gradient-stops))',
+        'trees': "url('https://images.unsplash.com/photo-1575489272413-cb506258027e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')",
+        'moon': "url('https://images.unsplash.com/photo-1447433589675-4aaa569f3e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80')",
+        'texture': "url('https://images.unsplash.com/photo-1487147264018-f937fba0c817?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80')"
       },
       colors: {
         'cherry-red': '#FF0000',
