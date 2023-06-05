20c6edc011001fa8beba5874b3104119c39bff15
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-06-05 15:32:40 -0500

    Update contrast images

--- app/page.tsx
+++ app/page.tsx
@@ -38,21 +38,21 @@ export default function Home() {
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-center p-24 ${contrastImage} bg-center bg-no-repeat`}>
-      <div className={"bg-red-100 p-10 rounded-md flex items-center flex-col"}>
+      <div className={"bg-red-100 p-3 rounded-md flex items-center flex-col"}>
         <p>Select Contrast Image</p>
         <form onChange={event => handleContrastImage(event)} className={'flex'}>
-          <input type="radio" id='trees' value='bg-trees' name='contrast_image'></input>
-          <label className={"p-5"} htmlFor='trees'>trees</label>
+          <input type="radio" id='bird' value='bg-bird' name='contrast_image'></input>
+          <label className={"p-5"} htmlFor='bird'>bird</label>
           <input type="radio" id='moon' value='bg-moon' name='contrast_image'></input>
           <label className={"p-5"} htmlFor='moon'>moon</label>
           <input type="radio" id='texture' value='bg-texture' name='contrast_image'></input>
-          <label className={"p-5"} htmlFor='texture'>texture</label>
+          <label className={"p-5"} htmlFor='texture'>lightbulbs</label>
           <input type="radio" id='none' value='' name='contrast_image'></input>
           <label className={"p-5"} htmlFor='none'>no image</label>
         </form>
       </div>
       <div id='circle' className={`rounded-full h-56 w-56 flex m-8 items-center justify-center ${colors[currentColor]}`}></div>
-      <div className={"bg-red-100 p-10 rounded-md"}>
+      <div className={"bg-red-100 p-5 rounded-md"}>
         <h1 className={`text-${colors[currentColor].slice(3)}`}>{currentHex}</h1>
         <input type="range" id="slider" name="slider"
           min="0" max="11" onChange={event => getColorValue(event)}>
